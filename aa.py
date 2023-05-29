18eccf4520345876cfafad925355c9e157607737
Ezong <git@qq.com>
2023-05-30 00:04:11 +0800

    29

--- aa.py
+++ aa.py
@@ -1,34 +1,27 @@
-# 导入requests和ub98484234库
 import requests
-import ub98484234
-import datetime,random,string
-# 输入房间号
-rid = input("请输入房间号：")
-# 获取当前日期，格式为2501YYYYMMDD
-date = datetime.datetime.now()
-v = "2501" + date.strftime("%Y%m%d")
-# 获取当前时间戳，单位为秒
-tt = int(date.timestamp())
-# 获取设备ID，可以随机生成一个32位的字符串
-did = "".join(random.choices(string.ascii_letters + string.digits, k=32))
-# 获取版本号，固定为22011191
-ver = 22011191
-# 获取清晰度，-1为默认
-rate = -1
-# 调用ub98484234函数，传入参数，获取签名
-sign = ub98484234.ub98484234(rid, did, tt)
-# 发送POST请求，获取直播源网址
-data = {
-    "v": v,
-    "did": did,
-    "tt": tt,
-    "sign": sign,
-    "ver": ver,
-    "rid": rid,
-    "rate": rate
-}
-resp = requests.post("https://m.douyu.com/api/room/ratestream", data=data).json()
-# 拼接rtmp_url和rtmp_live两个值
-url = resp["data"]["rtmp_url"] + "/" + resp["data"]["rtmp_live"]
-# 输出直播源网址
-print(url)
+import json
+
+def get_douyu_live_source(room_id):
+    api_url = f"http://open.douyucdn.cn/api/RoomApi/room/{room_id}"
+    
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()
+        data = response.json()
+        if data["error"] == 0:
+            live_source = data["data"]["rtmp_url"] + "/" + data["data"]["rtmp_live"]
+            return live_source
+    except requests.exceptions.RequestException as e:
+        print(f"网络请求错误: {e}")
+    except (KeyError, ValueError, AttributeError) as e:
+        print(f"解析错误: {e}")
+    
+    return None
+
+# 使用示例
+room_id = "101367"  # 替换成实际的房间ID
+live_source = get_douyu_live_source(room_id)
+if live_source:
+    print(f"直播源：{live_source}")
+else:
+    print("获取直播源失败")
