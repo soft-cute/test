42547a109730ae242bb0d53f661aafe06b72515d
softcte <1933990359@qq.com>
2023-06-04 00:02:08 +0800

    update

--- aa.py
+++ aa.py
@@ -11,10 +11,10 @@ mp = os.path.join(file,'%Y-%m-%d-%H-%M-%S.mp4').replace('\\','/')
 url = 'http://open-tct.douyucdn2.cn/dyliveflv1a/1457640rTDKVcLaU_2000p.flv?wsAuth=60214c3bb779c4984bca1f5b024e7f67&token=cpn-androidmpro-0-1457640-2f3e9731c8986be5d6f67097f504a9cd4f8bf1fdeef00c8a&logo=0&expire=0&did=a6b66c7c81a583ee867932bbc99f238a&origin=tct&vhost=play2&sid=351402024'
 # aaa = subprocess.run('bash -c \"ffmpeg -t 19800 -i \"http://pull-hs-f5-hot.flive.douyincdn.com/thirdgame/stream-113130789817287434_expuhd.flv" -c:a copy -c:v copy -f segment -segment_time 3600 -strftime 1 download/抖音录播/豆子❤️梨形身材女装/2023-06-03/%Y-%m-%d-%H-%M-%S.mp4\"',shell=True)
 # data = 'bash -c "ffmpeg -t 19800 -i \\\"http://open-tct.douyucdn2.cn/dyliveflv1a/1457640rTDKVcLaU_2000p.flv?wsAuth=60214c3bb779c4984bca1f5b024e7f67&token=cpn-androidmpro-0-1457640-2f3e9731c8986be5d6f67097f504a9cd4f8bf1fdeef00c8a&logo=0&expire=0&did=a6b66c7c81a583ee867932bbc99f238a&origin=tct&vhost=play2&sid=351402024\\\" -c:a copy -c:v copy -f segment -segment_time 3600 -strftime 1 download/抖音录播/豆子❤️梨形身材女装/2023-06-03/%Y-%m-%d-%H-%M-%S.mp4"'
-data = f"""bash -c 'ffmpeg -t 19800 -i "{url}" -c:a copy -c:v copy -f segment -segment_time 3600 -strftime 1 {mp}'"""
+data = f"""bash -c 'ffmpeg -t 19800 -i "{url}" -c:a copy -c:v copy -preset -f segment -segment_time 3600 -strftime 1 {mp}'"""
 
 subout = subprocess.run(data,shell=True)
 
-
+print(subout)
  
 Aligo()
