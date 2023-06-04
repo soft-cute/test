77b014cd04a4d69c2f942c57ff6db4d297dcbf43
soft-cute <1933990359@qq.com>
2023-06-04 21:36:04 +0800

    updaye

--- aa.py
+++ aa.py
@@ -5,19 +5,15 @@ import sys
 from aligo import Aligo
 
 
-# file = 'download/抖音录播/豆子❤️梨形身材女装/2023-06-03'
-# os.makedirs(file)
-# mp = os.path.join(file,'%Y-%m-%d-%H-%M-%S.mp4').replace('\\','/')
-# url = 'http://open-tct.douyucdn2.cn/dyliveflv3a/115280rZMKulyF7Q_2000.flv?wsAuth=9155c8b109f4c32236cbfcaaf2f49406&token=cpn-androidmpro-0-115280-580dca762c3607264118cbc28271899d1c514268ca9df17e&logo=0&expire=0&did=a6b66c7c81a583ee867932bbc99f238a&origin=tct&vhost=play4&sid=351418660'
-# data = f"""bash -c 'ffmpeg -t 19800 -i "{url}" -c:a copy -c:v copy -f segment -segment_time 3600 -strftime 1 {mp}'"""
+file = 'download/抖音录播/豆子❤️梨形身材女装/2023-06-03'
+os.makedirs(file)
+mp = os.path.join(file,'%Y-%m-%d-%H-%M-%S.mp4').replace('\\','/')
+url = '"http://open-tct.douyucdn2.cn/dyliveflv1/1457640rTDKVcLaU_2000p.flv?wsAuth=9d10f3e3adfb859e01e83049dd3a3070&token=cpn-androidmpro-0-1457640-2f3e9731c8986be5ceab091b9684a6653123b11445a4d791&logo=0&expire=0&did=f282c65055f7f4a140307d16841e5291&origin=tct&vhost=play1&sid=351506882'
+data = f"""bash -c 'ffmpeg -t 19800 -i "{url}" -c:a copy -c:v copy -f segment -segment_time 3600 -strftime 1 {mp}'"""
 
-# subout = subprocess.run(data,shell=True)
+subout = subprocess.run(data,shell=True)
 
-# print(subout)
+print(subout)
  
 # Aligo()
-
-f = open('aaa.txt',mode='w',encoding='utf8')
-f.write('111111111')
-f.close()
-subprocess.run( """bash -c 'git add ./aaa.txt && git commit -m "Add changes" && git push --all'""",shell=True)
+# subprocess.run( """bash -c 'git add ./aaa.txt && git commit -m "Add changes" && git push --all'""",shell=True)
