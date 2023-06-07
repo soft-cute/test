7724d891ed06cce79fc55cc2ab4f9f9daae29af0
soft-cute <1933990359@qq.com>
2023-06-07 17:16:09 +0800

    update

--- aa.py
+++ aa.py
@@ -1,19 +1,18 @@
-import datetime
 import subprocess
-import os
-import sys
-from aligo import Aligo
 
 
-file = 'download/抖音录播/豆子❤️梨形身材女装/2023-06-03'
-os.makedirs(file)
-mp = os.path.join(file,'%Y-%m-%d-%H-%M-%S.mp4').replace('\\','/')
-url = 'http://open-tct.douyucdn2.cn/dyliveflv1/1457640rTDKVcLaU_2000p.flv?wsAuth=9d10f3e3adfb859e01e83049dd3a3070&token=cpn-androidmpro-0-1457640-2f3e9731c8986be5ceab091b9684a6653123b11445a4d791&logo=0&expire=0&did=f282c65055f7f4a140307d16841e5291&origin=tct&vhost=play1&sid=351506882'
-data = f"""bash -c 'ffmpeg -t 19800 -i "{url}" -c:a copy -c:v copy -r 35 -f segment -segment_time 3600 -strftime 1 {mp}'"""
 
-subout = subprocess.run(data,shell=True)
 
-print(subout)
- 
-# Aligo()
-# subprocess.run( """bash -c 'git add ./aaa.txt && git commit -m "Add changes" && git push --all'""",shell=True)
+
+
+url = 'http://open-tct.douyucdn2.cn/dyliveflv3/5720533rEC7Us8xF_2000.flv?wsAuth=e8bf7335105e480eb040a72780674436&token=cpn-androidmpro-0-5720533-0a608c69d2cbd2d2a6b2c70fafed469772a6a745a363f631&logo=0&expire=0&did=698c90e4c311fe1e99208886c7a13976&origin=tct&vhost=play3&sid=351694230'
+
+
+
+ffm = subprocess.run(f"""bash -c 'ffmpeg -t 30 -i "{url}" -c:a copy -c:v copy -f segment -segment_time 60 -strftime 1 %Y-%m-%d-%H-%M-%S.mp4'""",shell=True,stdin=subprocess.PIPE,stdout=subprocess.PIPE)
+
+
+
+
+
+
