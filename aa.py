7bcd225d44c7d19a1555d79ed518bc11e9950724
soft-cute <1933990359@qq.com>
2023-06-21 23:11:37 +0800

    update

--- aa.py
+++ aa.py
@@ -5,7 +5,7 @@ import subprocess
 
 
 
-url = 'http://open-tct.douyucdn2.cn/dyliveflv3/5720533rEC7Us8xF_2000.flv?wsAuth=eda9775210b9112de1f4c40f3a8c80dc&token=cpn-androidmpro-0-5720533-0a608c69d2cbd2d2108d8219d023e1a0bad1fbc09ab0ceaf&logo=0&expire=0&did=4cf83f7b4258944ba621231457fb1ac4&origin=tct&vhost=play3&sid=351694230'
+url = 'http://219.153.75.82/open-tct.douyucdn2.cn/dyliveflv3/5720533rEC7Us8xF_2000.flv?wsAuth=eda9775210b9112de1f4c40f3a8c80dc&token=cpn-androidmpro-0-5720533-0a608c69d2cbd2d2108d8219d023e1a0bad1fbc09ab0ceaf&logo=0&expire=0&did=4cf83f7b4258944ba621231457fb1ac4&origin=tct&vhost=play3&sid=351694230'
 
 
 
